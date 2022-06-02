b1eeb7dcdc57d5b19120429e43da6fdfa010f601
ShunichirouKamino <kamino.shunichirou@isid.co.jp>
2022-06-02 23:24:11 +0900

    add enum from string

--- harp/src/ddl/field_types.rs
+++ harp/src/ddl/field_types.rs
@@ -1,46 +1,58 @@
-#[derive(Clone, PartialEq, Eq, Debug)]
+use strum::EnumString;
+
+#[derive(Clone, PartialEq, Eq, Debug, EnumString)]
 pub enum FieldType {
-    NumberField(NumberField),
-    StringField(StringField),
-    DateField(DateField),
+    // Number types
+    #[strum(serialize = "bit")]
+    Bit,
+    #[strum(serialize = "tinyiny")]
+    Tinyiny,
+    #[strum(serialize = "bool")]
+    Bool,
+    #[strum(serialize = "smallint")]
+    SmallInt,
+    #[strum(serialize = "mediumint")]
+    MediumInt,
+    #[strum(serialize = "int")]
+    Int,
+    #[strum(serialize = "bigint")]
+    BigInt,
+    #[strum(serialize = "decimal")]
+    Decimal,
+    #[strum(serialize = "float")]
+    Float,
+    #[strum(serialize = "double")]
+    Double,
+
+    // String types
+    #[strum(serialize = "char")]
+    Char(u64),
+    #[strum(serialize = "varchar")]
+    Varchar(u64),
+    #[strum(serialize = "binary")]
+    Binary(u64),
+    #[strum(serialize = "varbinary")]
+    VarBinary(u64),
+    #[strum(serialize = "blob")]
+    Blob,
+    #[strum(serialize = "text")]
+    Text,
+
+    // Date types
+    #[strum(serialize = "date")]
+    Date,
+    #[strum(serialize = "time")]
+    Time,
+    #[strum(serialize = "datetime")]
+    DateTime,
+    #[strum(serialize = "timestamp")]
+    TimeStamp,
+    #[strum(serialize = "year")]
+    Year,
 }
 
 impl Default for FieldType {
     fn default() -> Self {
-        FieldType::StringField(StringField::Varchar)
+        FieldType::Varchar(128)
     }
-}
-
-#[derive(Clone, PartialEq, Eq, Debug)]
-pub enum NumberField {
-    Bit,
-    Tinyiny,
-    Bool,
-    SmallInt,
-    MediumInt,
-    Int,
-    Integer,
-    BigInt,
-    Decimal,
-    Float,
-    Double,
-}
-
-#[derive(Clone, PartialEq, Eq, Debug)]
-pub enum StringField {
-    Char,
-    Varchar,
-    Binary,
-    VarBinary,
-    Blob,
-    Text,
-}
-
-#[derive(Clone, PartialEq, Eq, Debug)]
-pub enum DateField {
-    Date,
-    Time,
-    DateTime,
-    TimeStamp,
-    Year,
 }
